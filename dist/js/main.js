10cb32f62c2d876ca6a9e103dec6d1a73c3ce601
Vincent Ramirez <vincentcanonizadoramirez@gmail.com>
2022-02-10 14:30:10 +0800

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+const profileDropdown = document.querySelector('.user-profile');
+const profileOptions = document.querySelector('.user-profile__options');
+
+// profileDropdown.addEventListener('mouseover', () => {
+//   profileOptions.classList.add('dropdown-show');
+//   console.log('fired');
+// });
